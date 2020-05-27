53f32bd0d92d8acbabd0f042730fb9ef1f938ba0
adam iqshan <omai.mohmed9@gmail.com>
2020-05-27 15:15:10 +0530

    'Dice Game Finished Project'

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,1 +1,36 @@
-document.query
+// Modify Dice 1
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+var image1 = document.querySelectorAll('img')[0];
+
+image1.setAttribute('src',randomImageSource1);
+
+// Modify Dice 2
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice"+ randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll('img')[1];
+
+image2.setAttribute('src', randomImageSource2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector('h1').style.fontSize = "6rem"
+    document.querySelector('h1').innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2>randomNumber1) {
+    document.querySelector('h1').style.fontSize = "6rem"
+    document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩";
+}
+
+else{
+    document.querySelector('h1').innerHTML = 'Draw!';
+}
