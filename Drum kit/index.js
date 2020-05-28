43429bb6e99b5c4b393bea941705fdaa3edcbe98
adam iqshan <omai.mohmed9@gmail.com>
2020-05-28 22:34:43 +0530

    'Initial Commit'

--- Drum kit/index.js
+++ Drum kit/index.js
@@ -1,4 +1,9 @@
-document.querySelector('button').addEventListener('click', playsound);
+var drumBtns = document.querySelectorAll('.drum');
+
+for(var i=0; i<drumBtns.length; i++){
+    drumBtns[i].addEventListener('click', playsound)
+}
+
 
 function playsound() {
     alert('Ahem...You clicked me!')
