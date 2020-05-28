f333217c556c345186f1db7613584038bf3391ab
adam iqshan <omai.mohmed9@gmail.com>
2020-05-29 03:31:54 +0530

    'added images to buttons'

--- Drum kit/index.js
+++ Drum kit/index.js
@@ -6,5 +6,12 @@ for(var i=0; i<drumBtns.length; i++){
 
 
 function playsound() {
-    alert('Ahem...You clicked me!')
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
+
+for(var i=0; i<drumBtns.length; i++)
+{
+    drumBtns[i].addEventListener('click', function(){
+    this.style.color = 'white';});
+};
