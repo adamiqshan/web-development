b00816f70f2c6daf1f1ea4135e229c6160eb8a6c
adam iqshan <omai.mohmed9@gmail.com>
2020-05-29 16:21:19 +0530

    'finished project'

--- Drum kit/index.js
+++ Drum kit/index.js
@@ -1,17 +1,67 @@
-var drumBtns = document.querySelectorAll('.drum');
+var drumBtns = document.querySelectorAll(".drum");
 
-for(var i=0; i<drumBtns.length; i++){
-    drumBtns[i].addEventListener('click', playsound)
+// function playsound() {
+//   var audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
+// }
+
+// for (var i = 0; i < drumBtns.length; i++) {
+//   drumBtns[i].addEventListener("click", playsound);
+// }
+
+for (var i = 0; i < drumBtns.length; i++) {
+  drumBtns[i].addEventListener("click", function () {
+    var btn = this.innerHTML;
+    makesound(btn);
+    animater(btn);
+  });
+
+  document.addEventListener("keypress", function (event) {
+    makesound(event.key);
+    animater(event.key);
+  });
 }
 
-
-function playsound() {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+  }
 }
 
-for(var i=0; i<drumBtns.length; i++)
-{
-    drumBtns[i].addEventListener('click', function(){
-    this.style.color = 'white';});
+function animater(key){
+    var pressedkey = document.querySelector("."+key);
+
+    pressedkey.classList.add("pressed");
+
+    setTimeout(function(){
+        pressedkey.classList.remove("pressed");
+    }, 100);
+
 };
