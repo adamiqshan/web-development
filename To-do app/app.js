0b723db809ead0595ba620a45916cb1e9f14d458
adam iqshan <omai.mohmed9@gmail.com>
2020-07-12 16:11:42 +0530

    'built scoob page - fcc project#1'

--- To-do app/app.js
+++ To-do app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose')
 const date = require(__dirname+'/date.js');
 
 const app = express();
@@ -8,9 +9,25 @@ app.set('view engine', 'ejs' );
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-var items = ['Kafka on the shore', 'Python', 'Alchemist'];
-let workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB',  { useNewUrlParser: true,  useUnifiedTopology: true })
 
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+    name: 'Kafka on the shore'
+});
+const item2 = new Item({
+    name: 'Alchemist'
+});
+const item3 = new Item({
+    name: 'Few things left unsaid'
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get('/', function(req, res){
     
@@ -22,19 +39,56 @@ app.get('/', function(req, res){
         var day = today.toLocaleDateString('en-US', option);
     }
 
-    res.render('list', {listTitle: day, AddItem:items});
+    Item.find({}, function(err, founditems){
+        if (founditems.length ===0){
+            Item.insertMany(defaultItems, function(err, itms){
+                if (err){
+                    console.log(err)
+                } else {
+                    console.log('instantiated with '+ itms.length +' default values')
+                }
+            });
+                res.redirect('/')
+        } else{
+            res.render('list', {listTitle: day, AddItem:founditems});
+        }
+    });
+ 
+});
+
+app.get('/:customList', function(req, res){
+    console.log(req.params.customList);
+});
+
+app.post('/delete', function(req, res){
+    // model.findOneAndDelete() should be able to delete on _id
+    Item.deleteOne({_id:req.body.checkbox}, function(err){
+        if (err){
+            console.log(err)
+        } else {
+            console.log("successfully deleted entry from db")
+        }
+    });
+    res.redirect('/')
 });
 
 app.post('/', function(req, res){
-    item = req.body.newItem;
+    const itemName = req.body.newItem;
 
-    if (req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect('/work')
-    } else{
-        items.push(item);
-        res.redirect('/')
-    }
+    const itemX = new Item({
+        name:itemName
+    });
+
+    itemX.save();
+    res.redirect('/');
+
+    // if (req.body.list === "Work"){
+    //     workItems.push(item);
+    //     res.redirect('/work')
+    // } else{
+    //     items.push(item);
+    //     res.redirect('/')
+    // }
     
 })
 
