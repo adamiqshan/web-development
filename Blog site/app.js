c1ab89f1282d8d613caa01e331072d946897cf92
adam iqshan <omai.mohmed9@gmail.com>
2020-07-25 21:46:02 +0530

    'mongoDB intergrated'

--- Blog site/app.js
+++ Blog site/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const https = require("https");
 const lodash = require('lodash')
+const mongoose = require('mongoose')
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,15 +15,25 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/blogsDB',  { useNewUrlParser: true,  useUnifiedTopology: true });
+
+const postSchema  = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
 app.get("/", function (req, res) {
-  res.render("home", { StartingContent: homeStartingContent, posts:posts });
+  Post.find({}, function(err, posts){
+    res.render("home", { StartingContent: homeStartingContent, posts:posts });
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -37,28 +48,34 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.get('/posts/:postName', function(req, res){
-  var requestedTitle = req.params.postName;
-  requestedTitle = lodash.lowerCase(requestedTitle)
+app.get('/posts/:postId', function(req, res){
+  var requestedPostId = req.params.postId;
+  // requestedTitle = lodash.lowerCase(requestedTitle)
 
-  posts.forEach(function(post){
-    var storedTitle = lodash.lowerCase(post.title);
+  Post.findOne({_id:requestedPostId}, function(err, post){
+    res.render('post', {
+      displayTitle:post.title,
+      displayPost: post.content });
+  })
 
-    if (storedTitle === requestedTitle){
-      res.render('post', {
-        displayTitle:post.title,
-        displayPost: post.content });
-    };
-  });
+  // posts.forEach(function(post){
+  //   var storedTitle = lodash.lowerCase(post.title);
+
+  //   if (storedTitle === requestedTitle){
+  //     res.render('post', {
+  //       displayTitle:post.title,
+  //       displayPost: post.content });
+  //   };
+  // });
 });
 
 app.post("/compose", function (req, res) {
-   const post = { 
+   const post = new Post({ 
     title: req.body.postTitle, 
     content: req.body.postBody 
-  };
+  });
 
-  posts.push(post);
+  post.save();
   res.redirect('/');
 });
 
