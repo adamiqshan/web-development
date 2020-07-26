a455eed724ea39ca120826a97e7428dabf4aa211
adam iqshan <omai.mohmed9@gmail.com>
2020-07-27 01:37:08 +0530

    'REST API integrated'

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -14,14 +14,19 @@ mongoose.connect('mongodb://localhost:27017/wikiDB',  { useNewUrlParser: true,  
 
 const articleSchema = new mongoose.Schema({
     title: String,
-    content: String
-});
+    content: String },
+    { versionKey: false}
+    );
 
 const Article = mongoose.model('Article', articleSchema);
 
 //API CALLS
 
-app.get('/articles', function(req, res){
+//chained calls to search all articles
+
+app.route('/articles')
+
+.get(function(req, res){
     Article.find({}, function(err, foundItems){
         if(!err){
             res.send(foundItems);
@@ -29,9 +34,8 @@ app.get('/articles', function(req, res){
             res.send(err);
         }
     } );
-});
-
-app.post('/articles', function(req, res){
+})
+.post(function(req, res){
     const article = new Article({
         title:req.body.title, 
         content: req.body.content});
@@ -44,6 +48,90 @@ app.post('/articles', function(req, res){
         }
     });
 })
+.delete(function(req, res){
+    Article.deleteMany({}, function(err){
+        if(!err){
+            res.send('succesfully wiped out your DB');
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+
+// app.get('/articles', function(req, res){
+//     Article.find({}, function(err, foundItems){
+//         if(!err){
+//             res.send(foundItems);
+//         } else {
+//             res.send(err);
+//         }
+//     } );
+// });
+
+// app.post('/articles', function(req, res){
+//     const article = new Article({
+//         title:req.body.title, 
+//         content: req.body.content});
+
+//     article.save(function(err){
+//         if(!err){
+//             res.send("added new article succesfully!");
+//         } else{
+//             res.send(err);
+//         }
+//     });
+// });
+
+// app.delete('/articles',function(req, res){
+//     Article.deleteMany({}, function(err){
+//         if(!err){
+//             res.send('succesfully wiped out your DB');
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
+
+//chained calls for specific articles
+
+app.route('/articles/:articleName')
+
+.get(function(req, res){
+    Article.findOne({title: req.params.articleName}, function(err, matchingArticle){
+        if(!err){
+            res.send(matchingArticle);
+        } else {
+            res.send('No such article was found!')
+        }
+    });
+})
+
+.put(function(req, res){
+    Article.update({ title: req.params.articleName }, { title: req.body.title, content: req.body.content }, { overwrite: true }, function(err) {
+        if (!err) {
+            res.send("Successfully updated");
+        }
+    });
+})
+
+.patch(function(req, res){
+    Article.update({title:req.params.articleName}, {$set: req.body}, function(err){
+        if(!err){
+            res.send('Successfully patched!')
+        }
+    });
+})
+
+.delete(function(req, res){
+    Article.deleteOne({title:req.params.articleName}, function(err){
+        if (!err){
+            res.send('Successfully removed the article')
+        } else {
+            res.send('there was an error!')
+        }
+    })
+});
 
 app.listen(3000, function(){
     console.log('server running on port 3000');
