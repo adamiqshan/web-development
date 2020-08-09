9d999255e4d6be430246a57c5aea5672e2d54fa2
adam iqshan <omai.mohmed9@gmail.com>
2020-08-09 22:30:22 +0530

    'Added Session Cookies'

--- SecretsApp/app.js
+++ SecretsApp/app.js
@@ -6,8 +6,12 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 // const encrypt = require('mongoose-encryption');
 // const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRounds = 10; 
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+const passport = require("passport");
+const session = require('express-session')
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
@@ -15,7 +19,17 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'Oh this is cool',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -27,7 +41,14 @@ const userSchema = new mongoose.Schema({
 // const encKey = process.env.ENCKEY
 // userSchema.plugin(encrypt, {secret: encKey, encryptedFields: ['password']});
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res){
     res.render('home')
@@ -41,7 +62,62 @@ app.get('/login', function(req, res){
     res.render('login')
 });
 
+app.get('/secrets', function (req, res) {
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
+});
 
+app.get('/logout', function(req, res){
+    req.logout()
+    res.redirect('/')
+});
+
+
+app.post('/register', function(req, res){
+    User.register({username:req.body.username}, req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets')
+            });
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+
+// about using username, password in the schema instead of using email, password
+
+/*"MongoDB is a NoSQL database and none of the fields are required. So, even though you defined a schema, you aren't forced to follow it like you would be with a SQL database. When you register your user, you save the username as:
+User.register({username: req.body.username} ...
+Because of this, the "req.body.username" is saved to a username field and the email field that we had specified before goes uncreated.
+If you take a look at your MongoDB database, you should only have a "username" and "password" fields for these entries."*/ 
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
+        
+    })
+
+});
+
+
+/*Bcrypt
 app.post('/register', function(req, res){    
     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 
@@ -80,7 +156,7 @@ app.post('/login', function(req, res){
         }
     })
 });
-
+*/
 
 app.listen(3000, function(){
     console.log('server running on port 3000')
